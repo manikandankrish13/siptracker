ef9b4229bb1fe494a80b508942595f18eaa0b21e
manikandankrish13 <manikandankrish13@gmail.com>
2026-06-08 10:06:49 +0530

    Add Firebase configuration placeholder

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+// Add your Firebase web configuration here
+// Values should come from Vercel environment variables
+
+export const firebaseSettings={
+ apiKey:'',
+ authDomain:'',
+ projectId:'',
+ storageBucket:''
+};
